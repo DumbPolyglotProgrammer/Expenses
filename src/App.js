8561d0327669b6187ad8380c5a640ba545116fb8
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2021-06-06 15:58:36 +0530

    JSX under the hood compiled comment added

--- src/App.js
+++ src/App.js
@@ -48,6 +48,13 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 }
 
 export default App;
