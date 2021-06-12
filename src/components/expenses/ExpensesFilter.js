485133b2b1c8a9adc16bd9598bf6ca02a0a978f6
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2021-06-12 20:04:52 +0530

    Controlled Component comment

--- src/components/expenses/ExpensesFilter.js
+++ src/components/expenses/ExpensesFilter.js
@@ -3,6 +3,9 @@ import React from "react";
 import "./ExpensesFilter.css";
 
 // Controlled Component
+// A Controlled Component is one that takes its current value through props and notifies changes through callbacks like onChange.
+// A parent component "controls" it by handling the callback and managing its own state and passing the new values as props to the controlled component.
+// You could also call this a "dumb component".
 
 const ExpensesFilter = (props) => {
   const dropdownChangeHandler = (event) => {
