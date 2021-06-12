5e11c45f88c5179c450a83c099bbbe2cce29f181
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2021-06-12 19:58:40 +0530

    Section 4 complete

--- src/components/new_expense/NewExpense.js
+++ src/components/new_expense/NewExpense.js
@@ -4,10 +4,18 @@ import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      id: Math.random().toString(),
+      ...enteredExpenseData
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
